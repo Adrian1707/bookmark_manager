82f47e47359aab09e0ecab7c39683c503fb11935
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-22 11:39:57 +0100

    Set ENV to the top so capybara puts link in test database

--- app/app.rb
+++ app/app.rb
@@ -3,8 +3,13 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
-  get '/' do
-    'Hello BookmarkManager!'
+  # get '/' do
+  #   'Hello BookmarkManager!'
+  # end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
