c5b1f7ebf4c86a2f13a16b8ce5c4dcb0648a63b1
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-22 10:28:19 +0100

    Add config file, create app directory

--- Gemfile
+++ Gemfile
@@ -3,5 +3,8 @@ source "https://rubygems.org"
 
 gem 'rspec'
 gem 'capybara'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
 
 # gem "rails"
---
+++ app/app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/' do
+    'Hello BookmarkManager!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
