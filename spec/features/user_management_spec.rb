4aadb72b6b53c8a698a8f85c8662477b8c67c1e4
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-23 21:01:24 +0100

    Test for matching password now passing

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,11 +8,11 @@ feature 'User sign up' do
   end
 
   scenario 'requires a matching confirmation password' do
-    expect{ sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect{ sign_up(password_confirmation: 'wrong password') }.not_to change(User, :count)
   end
 
   def sign_up(email: 'alice@example.com',
-              password: '1234567',
+              password: '12345678',
               password_confirmation: '12345678')
     visit '/users/new'
     fill_in :email,    with: email
