9465754ed0f0daf9a8db8aab2c908f1c4f65d0f4
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-24 16:17:01 +0100

    Add test for signing in with correct credentials

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,3 +1,26 @@
+feature 'User sign in' do
+
+  let (:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email, password: user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
+  end
+
+  def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in email:
+    fill_in password:
+    click_button 'Sign in'
+  end
+
+end
+
+
 
 feature 'User sign up' do
 
