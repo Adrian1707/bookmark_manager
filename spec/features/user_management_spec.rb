f26e4d02e6946257721106794da953f3fb964db5
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-24 20:27:54 +0100

    Add test for while being signed in

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -20,6 +20,23 @@ feature 'User sign in' do
 
 end
 
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(email: 'test@test.com',
+                password: 'test',
+                password_confirmation: 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in(email: 'test@test.com', password: 'test')
+    click_button 'Sign out'
+    expect(page).to have_content('goodbye!')
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+  
+end
+
 feature 'User sign up' do
 
   scenario 'I can sign up as a new user' do
